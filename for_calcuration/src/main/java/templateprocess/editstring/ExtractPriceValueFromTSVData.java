e6cbb243f982d86f3a42f66d079fef8928d2a283
yamaoka daisuke <oswald_boc@icloud.com>
2018-02-28 02:24:58 +0900

    clear 冗長なメソッドを削除、定数名の修正

--- for_calcuration/src/main/java/templateprocess/editstring/ExtractPriceValueFromTSVData.java
+++ for_calcuration/src/main/java/templateprocess/editstring/ExtractPriceValueFromTSVData.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public class ExtractPriceValueFromTSVData implements ManipulateString {
 
-    private final int INDEX_OF_PRICE_PLACE = 4;
+    private final int INDEX_OF_PRICE = 4;
     
     private final String TAB = "\t";
 
@@ -12,17 +12,13 @@ public class ExtractPriceValueFromTSVData implements ManipulateString {
 
     @Override
     public String extractString(String value) {
-        String[] dividedValue = divideWithTab(value);
-        return dividedValue[INDEX_OF_PRICE_PLACE];
+        String[] dividedValue = value.split(TAB);
+        return dividedValue[INDEX_OF_PRICE];
     }
 
     @Override
     public String manipulateString(String value) {
         return replaceFromNonDiditToEmpty(value);
-    }
-
-    private String[] divideWithTab(String value) {
-        return value.split(TAB);
     }
 
     private String replaceFromNonDiditToEmpty(String value) {
