4c43a88d005331e9e4cbc693df2c6df647a3907c
yamaoka daisuke <oswald_boc@icloud.com>
2018-03-27 22:27:17 +0900

    clean 処理をシンプルにする_4

--- for_calcuration/src/main/java/com/icloud/oswald/model/rowdata/Text.java
+++ for_calcuration/src/main/java/com/icloud/oswald/model/rowdata/Text.java
@@ -1,9 +1,10 @@
-package com.icloud.oswald.model.rowdata.text;
+package com.icloud.oswald.model.rowdata;
 
 import java.util.List;
 import java.lang.SecurityException;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.nio.charset.StandardCharsets;
 import com.icloud.oswald.model.rowdata.Rowdata;
@@ -11,8 +12,9 @@ import com.icloud.oswald.model.rowdata.Rowdata;
 public class Text implements Rowdata {
 
     @Override
-    public List<String> getValues(Path value) throws IOException, SecurityException {
-        return Files.readAllLines(value, StandardCharsets.UTF_8);
+    public List<String> getValues(String value) throws IOException, SecurityException {
+        Path path = Paths.get(value);
+        return Files.readAllLines(path, StandardCharsets.UTF_8);
     }
 
 }
--- for_calcuration/src/main/java/com/icloud/oswald/service/facade/processes/MainProcess.java
+++ for_calcuration/src/main/java/com/icloud/oswald/service/facade/processes/MainProcess.java
@@ -5,8 +5,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import org.apache.commons.lang3.StringUtils;
-import com.icloud.oswald.model.rowdata.Rowdata;
-import com.icloud.oswald.model.rowdata.text.Text;
+import com.icloud.oswald.model.rowdata.Text;
 import com.icloud.oswald.service.facade.Facade;
 import com.icloud.oswald.service.validation.PriceValidation;
 
@@ -14,11 +13,15 @@ public class MainProcess implements Facade{
 
     private final String REGEXP_NON_DIGIT = "[^0-9]";
 
+    private final int PRICE_INDEX = 4;
+
+    private final String TAB = "\t";
+
     @Override
     public void execute(String value) {
         try {
             PriceValidation validate = new PriceValidation();
-            List<String> rows = read(value);
+            List<String> rows = new Text().getValues(value);
             int totalPrice = 0;
             for (String row : rows) {
                 if (!validate.hasPrice(row)) {
@@ -37,15 +40,8 @@ public class MainProcess implements Facade{
     }
 
     private String extract(String rowData) {
-        String[] values = rowData.split("\t");
-        String value = values[4].replaceAll(REGEXP_NON_DIGIT, StringUtils.EMPTY);
+        String[] values = rowData.split(TAB);
+        String value = values[PRICE_INDEX].replaceAll(REGEXP_NON_DIGIT, StringUtils.EMPTY);
         return value;
     }
-
-    private List<String> read(String value) throws Exception {
-        Path path = Paths.get(value);
-        Rowdata rowdata = new Text();
-        return rowdata.getValues(path);
-    }
-
 }
